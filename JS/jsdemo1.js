9f6388f32c08d16f5eeb8bfb1756ad2cd0d2798f
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-09-11 10:27:25 +0530

    js code examples

---
+++ JS/jsdemo1.js
@@ -0,0 +1,18 @@
+console.log("Hello Ashish");
+console.log(a);
+var a=5;
+console.log(a);
+{
+    console.log(a);
+}
+let b=10;
+const c=20;
+console.log(b);
+console.log(c);
+{
+    let d=30;
+  console.log(b);
+console.log(c);  
+console.log(d);
+}
+// console.log(d);
---
+++ JS/jsfunction.js
@@ -0,0 +1,10 @@
+// function sum(a,b){
+//     return a+b;
+// }
+let x=25;
+ const sum=(a=2,b=1)=>a+b; //arrow function
+  (function f1(){
+  console.log(`Hello ${x}`)
+  })();
+
+console.log("sum="+sum());
