bd2f830143bf842c42fa06c5b4f9025c2d87913f
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-09-23 16:59:55 +0530

    js fetch

--- JS/jsfetch.js
+++ JS/jsfetch.js
@@ -1,8 +1,15 @@
-const url="https://api.github.com/users/ashishvirgo"
-const res=fetch(url);
-res.then((response)=>{
-     return response.json();
-})
-.then((data)=>{
-    console.log("Data=",data);
-})
+const username='ashishvirgo';
+const url=`https://api.github.com/users/${username}`;
+const resp=fetch(url);
+    //   resp.then((res)=>{
+    //        return res.json();
+    //   })
+      resp.then((data)=>{
+        console.log("Data:",data);
+      })
+      .catch((err)=>{
+       console.log("Error:",err.message);
+      })
+      .finally(()=>{
+        console.log('Inside Finally block')
+      })
